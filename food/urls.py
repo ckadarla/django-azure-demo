697b8452839e7c8829bc800b3f3bcedfa58ef2ad
Chandra <smikcp@gmail.com>
2022-01-11 20:34:49 +0530

    update

--- food/urls.py
+++
@@ -1,9 +0,0 @@
-# food/urls.py
-from django.conf.urls import url
-from django.urls import path
-from food import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- food/views.py
+++
@@ -1,6 +0,0 @@
-# food/views.py
-from django.shortcuts import render, redirect
-
-
-def index(request):
-    return render(request, 'food/index.html', context=None)
